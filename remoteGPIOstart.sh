dbc9f051eaa4b00b274b222e33de000c9243cdea
Joshua <j.harris.kahn@gmail.com>
2012-11-12 19:17:46 -0800

    organizing

--- remoteGPIOstart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Execute the actual python script 
-# The "$@" passes in any parameters into the python exectuable 
-# The '&' puts the process into background (as a daemon) 
-# The 'echo $! > mydaemon.pid'  write the process id to a file
-
-python /usr/sbin/gpio_server.py "$@" &
-echo $! > /usr/sbin/remoteGPIOstart.pid
